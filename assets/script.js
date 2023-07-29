dacf8892e47f225d159ffaba462131a3826529e0
Atik <a71969541@gmail.com>
2023-07-29 15:34:22 +0600

    Login Tailwind

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+let mobileIcon = document.getElementById("mobileIcon");
+let mobileMenu = document.getElementById("mobileMenu");
+mobileIcon.addEventListener("click",()=>{
+	mobileMenu.classList.toggle("hidden");
+});
